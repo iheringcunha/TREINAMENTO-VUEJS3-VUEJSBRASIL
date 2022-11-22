6e2d1440a7d374283f7d1e25840a2e80788b098e
icunha <iheringcunha91@gmail.com>
2022-11-22 01:14:51 -0300

    live-vuejsbrasil

---
+++ live-vuejsbrasil/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 :style="{ color }">Hello {{ name }}</h1>
+    <input type="text" v-model="name" />
+    <button @click="handleClick">Alert!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      name: 'Ihering',
+      color: 'tomato'
+    }
+  },
+  methods: {
+    handleClick () {
+      window.alert('clicado')
+    }
+  }
+};
+</script>
