287221c138ef057178ae32ab19e0eb859b327284
Ihering Domingos Lima da Cunha <icunha@voxdata.local>
2022-11-25 11:27:47 -0300

    live-vuejsbrasil

--- live-vuejsbrasil/src/App.vue
+++ live-vuejsbrasil/src/App.vue
@@ -1,23 +1,40 @@
 <template>
-  <div>
+  <!-- <div>
     <h1 :style="{ color }">Hello {{ name }}</h1>
     <input type="text" v-model="name" />
     <button @click="handleClick">Alert!</button>
+  </div> -->
+
+  <div>
+    <h1>Minha lista</h1>
+    <input type="text" v-model="currentFood" />
+    <button @keyup.enter="addFood">Add</button>
+    <ul>
+      <li v-for="food in foods" :key="food">{{ food }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return{
-      name: 'Ihering',
-      color: 'tomato'
-    }
+    return {
+      // name: 'Ihering',
+      // color: 'tomato'
+
+      foods: ["maçã", "pêra"],
+      currentFood: "",
+    };
   },
   methods: {
-    handleClick () {
-      window.alert('clicado')
-    }
-  }
+    // handleClick () {
+    //   window.alert('clicado')
+    // }
+
+    addFood() {
+      this.foods.push(this.currentFood);
+      this.currentFood = "";
+    },
+  },
 };
 </script>
