7e8292499f024ac82f25a18cd0be082a1218a54b
icunha <iheringcunha91@gmail.com>
2022-12-01 06:47:01 -0300

    conceitos

---
+++ data-bind/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 :style="{{ textDecoadtion: decoration }}">Hello {{ name }}</h1>
+    <input type="text" v-model="name">
+    <br/>
+
+    <a href="link">Google</a>
+</template>
+
+<script>
+export default {
+data: () => ({
+    name: 'Ihering',
+    link: 'https://google.com',
+    decoration: 'underline'
+})
+}
+</script>
