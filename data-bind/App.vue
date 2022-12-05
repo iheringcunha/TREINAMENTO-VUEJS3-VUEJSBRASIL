759b8e4c68d321ab1e83e140e9d1c498a68f9607
Ihering Domingos Lima da Cunha <icunha@voxdata.local>
2022-12-05 13:35:27 -0300

    data-bind

---
+++ data-bind/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 :style="{ textDecoration: decoration }">Hello {{ name }}</h1>
+    <input type="text" v-model="name">
+    <br/>
+
+    <a :href="link">Google</a>
+  </div>
+</template>
+
+<script>
+export default {
+data: () => ({
+    name: 'Fulano',
+    decoration: 'underline',
+    link: 'http://google.com'
+})
+}
+</script>
+
+<style>
+
+</style>
