bef525ca262b11c1164eefcb3f30a8309ce1fbec
Ihering Domingos Lima da Cunha <icunha@voxdata.local>
2022-12-06 11:22:30 -0300

    diretivas

---
+++ diretivas/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Lista de tarefas !</h1>
+    <button @click="() => (showList = !showList)">Ver lista !</button>
+    <br />
+    <input type="text" v-focus />
+    <ul v-if="showList">
+      <li v-for="(task, index) in tasks" :key="`${task}-${index}`">
+        {{ task.name }}
+      </li>
+    </ul>
+    <p v-else>Lista de tarefas escondida !</p>
+  </div>
+</template>
+
+<script>
+const focus = {
+  inserted: (el) => {
+    el.focus();
+  },
+};
+export default {
+  directives: {
+    focus,
+  },
+  data: () => ({
+    showList: false,
+    tasks: [
+      {
+        name: "Fazer o curso",
+        isDone: false,
+      },
+    ],
+  }),
+};
+</script>
